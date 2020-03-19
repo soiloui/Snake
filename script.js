4c481174088ac35b68f7625942d1df6244765928
Mieczysław Palian <soilouiem@gmail.com>
2020-03-19 22:12:31 +0100

    Basics of mechanics game

--- script.js
+++ script.js
@@ -1,69 +1,159 @@
-const PLAY_POOL = document.querySelector('#pool');
-const SCORE = document.querySelector('#score');
-// const PLAYER_div = document.querySelector('#player');
-// const FOOD_div = document.querySelector("#food");
+`use strict`;
+const PLAY_POOL = document.querySelector("#pool");
+const SCORE = document.querySelector("#score");
 
-const rows_amount = getComputedStyle(PLAY_POOL).getPropertyValue('grid-template-rows').split(" ").length;
-const columns_amount = getComputedStyle(PLAY_POOL).getPropertyValue('grid-template-columns').split(" ").length;
-const total_amount = rows_amount*columns_amount;
+const rows_amount = getComputedStyle(PLAY_POOL)
+    .getPropertyValue("grid-template-rows")
+    .split(" ").length;
+const columns_amount = getComputedStyle(PLAY_POOL)
+    .getPropertyValue("grid-template-columns")
+    .split(" ").length;
+const total_amount = rows_amount * columns_amount;
+const player = document.createElement("div");
+const food = document.createElement("div");
 
 
-function GameStart(){
+let gameStatus = 0;
+let acceleration = 5;
+let speed = 1000;
+let direction = 'up';
+
+
+function GameStart() {
     boardCreate();
 
-    let player_position = Math.floor(Math.random()*total_amount);
+    let player_position = Math.floor(Math.random() * total_amount);
+
+    player.classList.add("play_pool--player");
+    gridAreaSet(player, player_position);
+    PLAY_POOL.appendChild(player);
+
     foodCreate(player_position);
+}
 
-    let player = document.createElement('div');
+function gridAreaSet(element, position) {
+    let row = Math.floor(position / columns_amount);
+    let col = position % columns_amount;
 
-    player.classList.add('play_pool--player');
-    PLAY_POOL.replaceChild(player, PLAY_POOL.children[player_position]);
+    if (row == 0) {
+        row = rows_amount;
+    }
+    if (col == 0) {
+        col = columns_amount;
+    }
 
-    let player_col = getComputedStyle(player).getPropertyValue('grid-column');
-    console.log(player_col);
-
-    // let direction = directionCalculate(player);
-    // console.log(direction);
+    element.style.gridArea = `${row} / ${col}`;
 }
 
 function boardCreate() {
-    for (let i = 0; i < rows_amount*columns_amount; i++){
-        const POOLS_div = document.createElement('div');
-        POOLS_div.classList.add('play_pool--pools');
+    for (let i = 1; i <= total_amount; i++) {
+        const POOLS_div = document.createElement("div");
+
+        POOLS_div.classList.add("play_pool--pools");
         PLAY_POOL.appendChild(POOLS_div);
+
+        gridAreaSet(POOLS_div, i);
     }
 }
 
-function foodCreate(player_pos) {
-    let food_position = Math.floor(Math.random()*rows_amount*columns_amount);
-    if (food_position == player_pos){
-        foodCreate(player_pos);
-    }
-    else{
-        let food = document.createElement('div');
-        food.classList.add('play_pool--food');
-        PLAY_POOL.replaceChild(food, PLAY_POOL.children[food_position]);
-    }
-}
+function foodCreate(player_position) {
+    let food_position = Math.floor(Math.random() * total_amount);
 
-function directionCalculate(player) {
-    let player_row = getComputedStyle(player).getPropertyValue('grid-row-start');
-    let player_col = getComputedStyle(player).getPropertyValue('grid-column-end');
+    if (food_position == player_position) {
+        foodCreate(player_position);
+    } else {
+        food.classList.add("play_pool--food");
+        PLAY_POOL.appendChild(food);
 
-    if (rows_amount > columns_amount){
-        if (player_row < rows_amount) return 'top';
-        else if (player_pos >= rows_amount) return 'bot';
-
-    } else if (rows_amount < columns_amount){
-        console.log(player_col);
-        if (player_col < columns_amount) return 'right';
-        else if (player_col >= columns_amount) return 'left';
-
-    } else if (rows_amount == columns_amount){
-        if (player_row < rows_amount) return 'top';
-        else if (player_pos >= rows_amount) return 'bot';
+        gridAreaSet(food, food_position);
     }
 }
 
+function directionCalculate(e) {
+    if (e.keyCode == 37) {
+        direction = 'left';
+    } else if (e.keyCode == 38) {
+        direction = 'up';
+    } else if (e.keyCode == 39) {
+        direction = 'right';
+    } else if (e.keyCode == 40) {
+        direction = 'down';
+    }
+
+    if (gameStatus == 0){
+        moveFunction();
+    }
+}
+
+function moveFunction() {
+    if (gameStatus == 0){
+        move = setInterval(function(){moveFunction()}, speed/acceleration);
+        gameStatus = 1;
+    }
+
+    let playerColumnStartNumber = parseInt(
+        getComputedStyle(player)
+        .getPropertyValue("grid-column-start")
+    );
+    let playerRowStartNumber = parseInt(
+        getComputedStyle(player)
+        .getPropertyValue("grid-row-start")
+    );
+
+    foodCollision(playerRowStartNumber, playerColumnStartNumber);
+
+    if (direction == "left") {
+        if (playerColumnStartNumber - 1 != 0) {
+            player.style.gridColumnStart = playerColumnStartNumber - 1;
+        } else breakGame();
+    }
+
+    if (direction == "up") {
+        if (playerRowStartNumber - 1 != 0) {
+            player.style.gridRowStart = playerRowStartNumber - 1;
+        } else breakGame();
+    }
+
+    if (direction == "right") {
+        if (playerColumnStartNumber + 1 != columns_amount + 1) {
+            player.style.gridColumnStart = playerColumnStartNumber + 1;
+        } else breakGame();
+    }
+
+    if (direction == "down") {
+        if (playerRowStartNumber + 1 != rows_amount + 1) {
+            player.style.gridRowStart = playerRowStartNumber + 1;
+        } else breakGame();
+    }
+}
+
+function foodCollision(pRow, pCol){
+    let foodColumnStartNumber = parseInt(
+        getComputedStyle(food)
+        .getPropertyValue("grid-column-start")
+    );
+    let foodRowStartNumber = parseInt(
+        getComputedStyle(food)
+        .getPropertyValue("grid-row-start")
+    );
+
+    if (pCol == foodColumnStartNumber
+        && pRow == foodRowStartNumber){
+            acceleration += 0.5;
+            PLAY_POOL.removeChild(food);
+            foodCreate();
+        }
+}
+
+function breakGame() {
+    clearInterval(move);
+    PLAY_POOL.removeChild(player);
+    PLAY_POOL.removeChild(food);
+    gameStatus = 0;
+    acceleration = 5;
+    GameStart();
+}
 
 GameStart();
+
+window.addEventListener("keydown", directionCalculate);
